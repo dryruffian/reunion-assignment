8153e32715a2e4130ef9a55dacce5a267f9699f4
Aditya raj <88555779+dryruffian@users.noreply.github.com>
2024-12-30 13:19:56 +0530

    intial commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,7 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript",{
+  ...compat.extends({
     extends: ['next'],
     rules: {
       'react/no-unescaped-entities': 'off',
